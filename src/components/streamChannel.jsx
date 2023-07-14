956a80015de00411311da1cd838f3ed9d651a120
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-14 15:46:03 +0200

    section stream channel

--- src/components/streamChannel.jsx
+++ src/components/streamChannel.jsx
@@ -2,13 +2,10 @@ import '../styles/streamChannel.css'
 import 'boxicons'
 
 const enlaces = [
-    { id: 1, href: '#', boxIcon: <box-icon size='sm' name='instagram' type='logo' color='#ffffff'/>},
-    { id: 2, href: '#', boxIcon: <box-icon size='sm' name='tiktok' type='logo' color='#ffffff' />},
-    { id: 3, href: '#', boxIcon: <box-icon size='sm' name='twitter' type='logo' color='#ffffff' />},
-    { id: 4, href: '#', boxIcon: <box-icon size='sm' name='twitch' type='logo' color='#ffffff' />},
-    { id: 5, href: '#', boxIcon: <box-icon size='sm' name='facebook-circle' type='logo' color='#ffffff' />},
-    { id: 6, href: '#', boxIcon: <box-icon size='sm' name='discord' type='logo' color='#ffffff' />},
-    { id: 7, href: '#', boxIcon: <box-icon size='sm' name='spotify' type='logo' color='#ffffff'/>}
+    { id: 1, href: '#', boxIcon: <box-icon size='sm' name='soundcloud' type='logo' color='#ffffff' />, name: 'SOUNCLOUD'},
+    { id: 2, href: '#', boxIcon: <box-icon size='sm' name='deezer' type='logo' color='#ffffff' />, name: 'DEEZER'},
+    { id: 3, href: '#', boxIcon: <box-icon size='sm' name='youtube' type='logo' color='#ffffff' />, name: 'YOUTUBE'},
+    { id: 7, href: '#', boxIcon: <box-icon size='sm' name='spotify' type='logo' color='#ffffff'/>, name: 'SPOTIFY'}
 ];
 
 export default function Stream(){
@@ -16,6 +13,7 @@ export default function Stream(){
             <li key={enlace.id}>
                 <a href={enlace.href}>
                     {enlace.boxIcon}
+                    {enlace.name}
                 </a>
             </li>
         );
