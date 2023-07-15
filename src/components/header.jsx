1c014f508833631b4f66fc568987c4f8dfe92223
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-15 13:41:48 +0200

    header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,15 @@
 import '../styles/header.css'
 import Logo from '../assets/music.svg'
+import SocialMedia from './socialMedia'
 
 
 export default function Header(){
     return(
         <header>
             <img src={Logo} alt="Logo" />
+            <ul className='header-menu'>
+                <SocialMedia/>
+            </ul>
             <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none">
                 <path d="M4 18L20 18" stroke="#ffffff" strokeWidth="2" strokeLinecap="round"/>
                 <path d="M4 12L20 12" stroke="#ffffff" strokeWidth="2" strokeLinecap="round"/>
