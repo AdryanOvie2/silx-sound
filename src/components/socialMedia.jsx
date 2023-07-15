b99abb0ca808cfb33ba34a8ee46c30ec2aa57035
AdryanOvie2 <adryanovie2@gmail.com>
2023-07-15 13:24:35 +0200

    socialMedia animation

--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -2,13 +2,13 @@ import '../styles/socialMedia.css'
 import 'boxicons'
 
 const links = [
-    { id: 1, href: '#', boxIcon: <box-icon size='sm' name='instagram' type='logo' color='#ffffff'/>},
-    { id: 2, href: '#', boxIcon: <box-icon size='sm' name='tiktok' type='logo' color='#ffffff' />},
-    { id: 3, href: '#', boxIcon: <box-icon size='sm' name='twitter' type='logo' color='#ffffff' />},
-    { id: 4, href: '#', boxIcon: <box-icon size='sm' name='twitch' type='logo' color='#ffffff' />},
-    { id: 5, href: '#', boxIcon: <box-icon size='sm' name='facebook-circle' type='logo' color='#ffffff' />},
-    { id: 6, href: '#', boxIcon: <box-icon size='sm' name='discord' type='logo' color='#ffffff' />},
-    { id: 7, href: '#', boxIcon: <box-icon size='sm' name='spotify' type='logo' color='#ffffff'/>}
+    { id: 1, href: '#', boxIcon: <box-icon size='sm' name='instagram' type='logo' color='#ffffff' animation='tada-hover'/>},
+    { id: 2, href: '#', boxIcon: <box-icon size='sm' name='tiktok' type='logo' color='#ffffff' animation='tada-hover' />},
+    { id: 3, href: '#', boxIcon: <box-icon size='sm' name='twitter' type='logo' color='#ffffff' animation='tada-hover' />},
+    { id: 4, href: '#', boxIcon: <box-icon size='sm' name='twitch' type='logo' color='#ffffff' animation='tada-hover' />},
+    { id: 5, href: '#', boxIcon: <box-icon size='sm' name='facebook-circle' type='logo' color='#ffffff' animation='tada-hover' />},
+    { id: 6, href: '#', boxIcon: <box-icon size='sm' name='discord' type='logo' color='#ffffff' animation='tada-hover' />},
+    { id: 7, href: '#', boxIcon: <box-icon size='sm' name='spotify' type='logo' color='#ffffff' animation='tada-hover'/>}
 ];
 
 export default function SocialMedia(){
